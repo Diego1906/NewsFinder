b652aa3fa12d1b57eaa2cfc3b61abc18bbbea7dc
Diego1906 <di_fsantos@yahoo.com.br>
2022-08-23 01:18:23 -0300

    Criação da classe de entidade

---
+++ app/src/main/java/com/example/newsfinder/domain/entities/ArticleEntity.kt
@@ -0,0 +1,23 @@
+package com.example.newsfinder.domain.entities
+
+data class ArticleEntity(
+    val status: String? = null,
+    val totalResults: Int,
+    val articles: List<ArticleItemEntity>? = null
+)
+
+data class ArticleItemEntity(
+    val source: ArticleSourceItemEntity? = null,
+    val author: String? = null,
+    val title: String? = null,
+    val description: String? = null,
+    val url: String? = null,
+    val urlToImage: String? = null,
+    val publishedAt: String? = null,
+    val content: String? = null
+)
+
+data class ArticleSourceItemEntity(
+    val id: String? = null,
+    val name: String? = null
+)
