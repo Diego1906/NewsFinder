401270398291c55cf2dd858505cbed40fcd47329
Diego1906 <di_fsantos@yahoo.com.br>
2022-08-23 01:18:04 -0300

    Movendo arquivo de local

--- app/src/main/java/com/example/newsfinder/presentation/MainActivity.kt
+++ app/src/main/java/com/example/newsfinder/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.newsfinder
+package com.example.newsfinder.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.newsfinder.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
