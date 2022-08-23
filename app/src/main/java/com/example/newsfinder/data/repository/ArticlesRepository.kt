504872473e6a90eb6249263b5a2be088f9be0ebe
Diego1906 <di_fsantos@yahoo.com.br>
2022-08-23 01:18:59 -0300

    Implementação inicial da classe de repositório

--- app/src/main/java/com/example/newsfinder/data/repository/ArticlesRepository.kt
+++ app/src/main/java/com/example/newsfinder/data/repository/ArticlesRepository.kt
@@ -1,4 +1,13 @@
 package com.example.newsfinder.data.repository
 
-class ArticlesRepository {
+import com.example.newsfinder.data.mappers.toEntity
+import com.example.newsfinder.data.remote.INewsApi
+import com.example.newsfinder.domain.entities.ArticleEntity
+import com.example.newsfinder.domain.repository.IArticlesRepository
+
+class ArticlesRepository(private val api: INewsApi) : IArticlesRepository {
+
+    override suspend fun getArticles(): ArticleEntity {
+        return api.getArticles(description = "", dateFrom = "").toEntity()
+    }
 }
