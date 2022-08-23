d3bfb5028196bddf437398e391722117c4eb435d
Diego1906 <di_fsantos@yahoo.com.br>
2022-08-23 00:45:31 -0300

    Configuração da API adicionando uma chamada de serviço

--- app/src/main/java/com/example/newsfinder/data/remote/INewsApi.kt
+++ app/src/main/java/com/example/newsfinder/data/remote/INewsApi.kt
@@ -1,3 +1,17 @@
 package com.example.newsfinder.data.remote
 
-interface INewsApi
+import com.example.newsfinder.BuildConfig
+import com.example.newsfinder.data.response.ArticleResponse
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface INewsApi {
+
+    @GET("everything")
+    suspend fun getArticles(
+        @Query("q") description: String,
+        @Query("from") from: String,
+        @Query("sortBy") sortBy: String = "publishedAt",
+        @Query("apiKey") apiKey: String = BuildConfig.API_KEY
+    ): ArticleResponse
+}
