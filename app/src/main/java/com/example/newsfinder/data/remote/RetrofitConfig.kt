28041df0310e61312fccdd2aa3f68e04e3530ba0
Diego1906 <di_fsantos@yahoo.com.br>
2022-08-24 21:43:01 -0300

    Implementação da configuração do retrofit

--- app/src/main/java/com/example/newsfinder/data/remote/RetrofitConfig.kt
+++ app/src/main/java/com/example/newsfinder/data/remote/RetrofitConfig.kt
@@ -1,4 +1,28 @@
 package com.example.newsfinder.data.remote
 
-class RetrofitConfig {
+import com.example.newsfinder.BuildConfig
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Inject
+
+class RetrofitConfig @Inject constructor() : IRetrofitConfig {
+
+    private val retrofit: Retrofit
+
+    init {
+        val moshi = Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+
+        retrofit = Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+    }
+
+    override fun getInstance(): INewsApi {
+        return retrofit.create(INewsApi::class.java)
+    }
 }
