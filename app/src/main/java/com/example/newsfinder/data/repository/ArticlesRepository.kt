6dc9ea60d5e897a2defdddd8e8d7f7269ebae02d
Diego1906 <di_fsantos@yahoo.com.br>
2022-08-24 21:43:55 -0300

    Ajuste da classe adicionando anotação do Hilt para usar D.I

--- app/src/main/java/com/example/newsfinder/data/repository/ArticlesRepository.kt
+++ app/src/main/java/com/example/newsfinder/data/repository/ArticlesRepository.kt
@@ -4,8 +4,11 @@ import com.example.newsfinder.data.mappers.toEntity
 import com.example.newsfinder.data.remote.INewsApi
 import com.example.newsfinder.domain.entities.ArticleEntity
 import com.example.newsfinder.domain.repository.IArticlesRepository
+import javax.inject.Inject
 
-class ArticlesRepository(private val api: INewsApi) : IArticlesRepository {
+class ArticlesRepository @Inject constructor(
+    private val api: INewsApi
+) : IArticlesRepository {
 
     override suspend fun getArticles(): ArticleEntity {
         return api.getArticles(description = "", dateFrom = "").toEntity()
