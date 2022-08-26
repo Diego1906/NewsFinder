2a3e441585e018a34030d4137c992455c5ef75e5
Diego1906 <di_fsantos@yahoo.com.br>
2022-08-25 22:46:22 -0300

    Mudar o tipo de interface de injeção

--- app/src/main/java/com/example/newsfinder/data/repository/ArticlesRepository.kt
+++ app/src/main/java/com/example/newsfinder/data/repository/ArticlesRepository.kt
@@ -1,16 +1,19 @@
 package com.example.newsfinder.data.repository
 
 import com.example.newsfinder.data.mappers.toEntity
-import com.example.newsfinder.data.remote.INewsApi
+import com.example.newsfinder.data.remote.IRetrofitConfig
 import com.example.newsfinder.domain.entities.ArticleEntity
 import com.example.newsfinder.domain.repository.IArticlesRepository
 import javax.inject.Inject
 
 class ArticlesRepository @Inject constructor(
-    private val api: INewsApi
+    private val api: IRetrofitConfig
 ) : IArticlesRepository {
 
     override suspend fun getArticles(): ArticleEntity {
-        return api.getArticles(description = "", dateFrom = "").toEntity()
+        return api.getInstance().getArticles(
+            description = "bmw",
+            dateFrom = "2022-07-25"
+        ).toEntity()
     }
 }
