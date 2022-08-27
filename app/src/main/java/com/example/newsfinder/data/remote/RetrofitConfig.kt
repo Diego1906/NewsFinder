72c03d2f4b6ea4872f6310fcbdd511a7dc47f105
Diego1906 <di_fsantos@yahoo.com.br>
2022-08-27 01:13:33 -0300

    Adição do Interceptor de log na classe de configuração do Retrofit

--- app/src/main/java/com/example/newsfinder/data/remote/RetrofitConfig.kt
+++ app/src/main/java/com/example/newsfinder/data/remote/RetrofitConfig.kt
@@ -3,6 +3,8 @@ package com.example.newsfinder.data.remote
 import com.example.newsfinder.BuildConfig
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import javax.inject.Inject
@@ -18,11 +20,20 @@ class RetrofitConfig @Inject constructor() : IRetrofitConfig {
 
         retrofit = Retrofit.Builder()
             .baseUrl(BuildConfig.BASE_URL)
+            .client(setupClient())
             .addConverterFactory(MoshiConverterFactory.create(moshi))
             .build()
     }
 
     override fun getInstance(): INewsApi {
         return retrofit.create(INewsApi::class.java)
+    }
+
+    private fun setupClient(): OkHttpClient {
+        HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY).also {
+            return OkHttpClient.Builder()
+                .addInterceptor(it)
+                .build()
+        }
     }
 }
