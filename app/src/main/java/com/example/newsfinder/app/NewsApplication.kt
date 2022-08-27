2e20bbbe7a936617d09cf2b01b19c4cea6d8b154
Diego1906 <di_fsantos@yahoo.com.br>
2022-08-27 02:23:32 -0300

    Implementação e configuração da lib Timber

--- app/src/main/java/com/example/newsfinder/app/NewsApplication.kt
+++ app/src/main/java/com/example/newsfinder/app/NewsApplication.kt
@@ -2,12 +2,14 @@ package com.example.newsfinder.app
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class NewsApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        Timber.plant(Timber.DebugTree())
     }
 
     companion object {
