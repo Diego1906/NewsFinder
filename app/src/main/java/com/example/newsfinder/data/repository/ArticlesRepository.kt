0b88f3e3def5e3d6279a63004716edb4346c9e44
Diego1906 <di_fsantos@yahoo.com.br>
2022-08-29 22:40:32 -0300

    Reorganização da classe alteração o tipo de parâmetro e a forma de acesso a API

--- app/src/main/java/com/example/newsfinder/data/repository/ArticlesRepository.kt
+++ app/src/main/java/com/example/newsfinder/data/repository/ArticlesRepository.kt
@@ -1,19 +1,14 @@
 package com.example.newsfinder.data.repository
 
 import com.example.newsfinder.data.mappers.toEntity
-import com.example.newsfinder.data.remote.IRetrofitConfig
+import com.example.newsfinder.data.remote.INewsApi
 import com.example.newsfinder.domain.entities.ArticlesEntity
 import com.example.newsfinder.domain.repository.IArticlesRepository
 import javax.inject.Inject
 
-class ArticlesRepository @Inject constructor(
-    private val api: IRetrofitConfig
-) : IArticlesRepository {
+class ArticlesRepository @Inject constructor(private val api: INewsApi) : IArticlesRepository {
 
     override suspend fun getArticles(): ArticlesEntity {
-        return api.getInstance().getArticles(
-            description = "bmw",
-            dateFrom = "2022-08-15"
-        ).toEntity()
+        return api.getArticles(description = "bmw", dateFrom = "2022-08-15").toEntity()
     }
 }
