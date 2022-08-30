1ac734ac8c113cbd580fe030696f73c2548d5f2f
Diego1906 <di_fsantos@yahoo.com.br>
2022-08-29 22:39:44 -0300

    Ajuste do módulo de injeção de dependência da camada de Data

--- app/src/main/java/com/example/newsfinder/data/di/DataModule.kt
+++ app/src/main/java/com/example/newsfinder/data/di/DataModule.kt
@@ -1,7 +1,7 @@
 package com.example.newsfinder.data.di
 
-import com.example.newsfinder.data.remote.IRetrofitConfig
-import com.example.newsfinder.data.remote.RetrofitConfig
+import com.example.newsfinder.data.remote.INewsApi
+import com.example.newsfinder.data.remote.ServiceConfig
 import com.example.newsfinder.data.repository.ArticlesRepository
 import com.example.newsfinder.domain.repository.IArticlesRepository
 import dagger.Module
@@ -14,8 +14,8 @@ import dagger.hilt.android.components.ActivityComponent
 object DataModule {
 
     @Provides
-    fun provideRetrofit(): IRetrofitConfig = RetrofitConfig()
+    fun provideService(): INewsApi = ServiceConfig().getInstance()
 
     @Provides
-    fun provideRepository(api: IRetrofitConfig): IArticlesRepository = ArticlesRepository(api)
+    fun provideRepository(api: INewsApi): IArticlesRepository = ArticlesRepository(api)
 }
